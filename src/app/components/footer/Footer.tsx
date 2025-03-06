9bec8d11691a40cdab4d08982c035ceacdb4008f
ahmed123humran <ahm88m%0@gmail.com>
2025-03-06 22:44:53 +0300

    [UPDATE] website responsive

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,16 +1,13 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { ContacUs } from '../home/contact_us';
-
 
 export default function Footer() {
     return (
         <section>
-            <footer className="bg-footer bg-cover lg:bg-contain bg-no-repeat bg-center lg:h-screen w-full pt-44 pb-10 px-5 lg:px-20">
-                <ContacUs />
+            <footer className="bg-primary lg:bg-transparent lg:bg-footer bg-cover lg:bg-cover bg-no-repeat lg:h-screen bg-center w-full py-8 lg:pt-56 px-5 lg:px-20">
                 <div className="flex justify-center items-center">
                     <div className="grid grid-cols-12 xl:mt-20">
-                        <div className="col-span-12 md:col-span-6">
+                        <div className="col-span-12 lg:col-span-6">
                             <Image src="/footer/marahel_logo.png" alt="..." width={250} height={10} />
                             <Image src="/footer/line1.png" alt="..." width={250} height={10} className="mt-3" />
                             <p className="text-white mt-5 text-justify lg:text-xl xl:text-2xl lg:ml-14">
@@ -29,19 +26,19 @@ export default function Footer() {
                             <Image src="/footer/line2.png" alt="..." width={100} height={10} className="mt-3" />
                             <div className="mt-5">
                                 <div className="flex items-center py-2 xl:px-6">
-                                    <div className="flex justify-center items-center">
-                                        <Image src="/footer/Button.png" className="w-10" alt="..." width={70} height={20} />
+                                    <div className="flex justify-center ">
+                                        <Image src="/footer/Button.png" className="" alt="..." width={32} height={20} />
                                     </div>
                                     <div className="mx-3 text-white">
                                         <h1 className="lg:text-lg xl:text-xl font-extrabold">الموقع</h1>
-                                        <p>
+                                        <p className="text-sm">
                                             8479 Al Mashaf, 3713  Ar Rabie Dist., Riyadh 13316 - KSA
                                         </p>
                                     </div>
                                 </div>
                                 <div className="flex items-center py-2 xl:px-6">
                                     <div className="flex justify-center items-center">
-                                        <Image src="/footer/Button (1).png" alt="..." width={30} height={20} />
+                                        <Image src="/footer/Button (1).png" alt="..." width={32} height={20} />
                                     </div>
                                     <div className="mx-3 text-white">
                                         <h1 className="font-extrabold lg:text-lg xl:text-xl">البريد الإلكتروني</h1>
@@ -52,7 +49,7 @@ export default function Footer() {
                                 </div>
                                 <div className="flex items-center py-2 xl:px-6">
                                     <div className="flex justify-center items-center">
-                                        <Image src="/footer/Button (2).png" alt="..." width={30} height={20} />
+                                        <Image src="/footer/Button (2).png" alt="..." width={32} height={20} />
                                     </div>
                                     <div className="mx-3 text-white">
                                         <h1 className="lg:text-lg xl:text-xlfont-extrabold ">رقم الهاتف</h1>
@@ -66,7 +63,7 @@ export default function Footer() {
                     </div>
                 </div>
             </footer>
-            <p className="text-center text-xl mx-10 -mt-5 lg:-mt-8 pb-5 text-primary">
+            <p className="text-center text-xl mx-10 pb-5 text-primary">
                 2025 © جقوق الطبع محفوظة لشركة مراحل الرقمية
             </p>
         </section>
--- src/app/components/home/service.tsx
+++ src/app/components/home/service.tsx
@@ -29,25 +29,25 @@ const services = [
 
 const Service = () => {
     return (
-        <section className="bg-service bg-cover bg-no-repeat bg-center lg:h-screen w-full text-white">
-            <div className="mx-2">
+        <section className="bg-primary lg:bg-transparent lg:bg-service bg-cover bg-no-repeat bg-center min-h-screen text-white">
+            <div className="mx-2 md:mx-10 lg:mx-20">
                 <h1 className="text-center text-3xl lg:text-5xl font-extrabold pt-20 xl:pt-32">خدماتنا</h1>
                 <div className="flex flex-wrap justify-center items-center">
-                    <div className="md:w-1/1 lg:w-1/3 pt-10 md:pt-0">
-                        <h1 className="text-4xl font-extrabold">نسعى دائمًـــا..</h1>
-                        <p className="mt-6 text-2xl text-justify md:ml-10">
+                    <div className="w-full lg:w-1/3 pt-10 md:pt-0">
+                        <h1 className="text-2xl lg:text-4xl  font-extrabold">نسعى دائمًـــا..</h1>
+                        <p className="mt-6 text-xl text-justify md:ml-10 pb-5 lg:pb-0">
                             بتسليط الضوء والتركيز بدقة على تقديم منتجات تقنية ورقمية تتسم بالإبداع والسلاسة مع توظيف فني وبديع للذكاء الاصطناعي (AI) مع المحافظة على مستوى عالٍ من الجودة لتقديم خدمات فريدة ومميزة لعملائنا.
                         </p>
                         <div className="pb-24 hidden lg:block">
                             <Image src="/service/Group 8.png" alt="..." height={10} width={250} />
                         </div>
                     </div>
-                    <div className="md:w-1/1 lg:w-1/2 lg:pt-24 xl:pt-20">
+                    <div className="w-full lg:w-2/3 lg:pt-24 xl:pt-32">
                         <div className="grid lg:grid-cols-2 gap-12">
                             {services.map((item, index) => (
                                 <div
                                     key={item.id}
-                                    className={`bg-white h-72  rounded-md shadow-2xl p-4 text-black ${index === 1 ? "lg:mt-16" : index === 2 ? "lg:-mt-16" : "mt-0"}`}
+                                    className={`bg-white h-72 rounded-md shadow-2xl p-4 text-black ${index === 1 ? "lg:mt-16" : index === 2 ? "lg:-mt-16" : "mt-0"}`}
                                 >
                                     <div className="flex justify-between">
                                         <div className="flex justify-center items-center mt-2">
@@ -67,8 +67,6 @@ const Service = () => {
                 </div>
             </div>
         </section>
-
-
     )
 }
 
