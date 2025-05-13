938d7f08f5c18be5dd8cbf843854c5f732cc05e0
ahmed123humran <ahm88m%0@gmail.com>
2025-05-13 11:26:44 +0300

    [UPDATE] marahel website

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -27,7 +27,7 @@ export default function Footer() {
                                     <hr className="border h-1.5 bg-info rounded-2xl border-info w-14 mt-2" />
                                 </div>
                             </div>
-                            <p className={`text-white mt-5 text-justify 2xl:text-2xl font-extrabold font-js ${isArabic ? "lg:ml-64" : "lg:mr-64"}`}>
+                            <p className={`text-white mt-5 text-justify 2xl:text-2xl font-extrabold font-js ${isArabic ? "lg:ml-16" : "lg:mr-16"}`}>
                                 {t('description')}
                             </p>
                         </div>
